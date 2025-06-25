71a5bdd47608dd3ea64d9216bfdf5160e3c48c71
Bibaswan Patra <bibaswan.patra@gmail.com>
2025-06-25 18:37:41 +0000

    Reinit at correct path

---
+++ target/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.singhdevhub","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"com.singhdevhub","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
